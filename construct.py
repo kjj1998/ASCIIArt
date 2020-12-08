99a696feeea37c9615da335556effb9f73409930
Koh Jun Jie <koh.jun.jie@outlook.com>
2020-12-08 16:40:54 +0800

    Added comments for the functions

--- construct.py
+++ construct.py
@@ -7,6 +7,8 @@ def construct_pixel_matrix(height, width, flattened_data):
     pixel_matrix, internal_matrix = [], []
     count = 0
 
+    # Inner loop constructs the pixel values within the row
+    # Outer loop constructs each row of pixel values
     for i in range(height):
         for j in range(width):
             internal_matrix.append(flattened_data[count])
@@ -22,7 +24,17 @@ def construct_pixel_matrix(height, width, flattened_data):
     return pixel_matrix
 
 def construct_brightness_matrix(height, width, pixel_matrix):
+   """ Function to construct the brightness matrix of the picture using the
+   pixel matrix
+   """
+
+   # Give the brightness matrix its structrue by padding it with zeroes
    brightness_matrix = [[0 for i in range(len(pixel_matrix[0]))] for j in range(len(pixel_matrix))]
+
+   # Convert each tuple of RGBA to a value that is the average of the four
+   # values, R, G, B and A
+   # Insert this average value into the appropriate position in the brightness
+   # matrix
    for x in range(len(pixel_matrix)):
    	for y in range(len(pixel_matrix[x])):
 	    pixel = pixel_matrix[x][y]
@@ -40,11 +52,19 @@ def construct_brightness_matrix(height, width, pixel_matrix):
    return brightness_matrix
 
 def construct_ascii_matrix(height, width, brightness_matrix, ASCII):
+    """ Function to construct the ascii matrix of the picture using the
+   brightness matrix
+   """ 
+
+    # Give the ascii matrix its structure by padding it with zeroes
     ASCII_matrix = [[0 for i in range(len(brightness_matrix[0]))] for j in range(len(brightness_matrix))]
+
+    # Convert each value in the brightness matrix to its corresponding
+    # character in the ASCII character set based on its density 
     for x in range(len(brightness_matrix)):
         for y in range(len(brightness_matrix[x])):
             ASCII_matrix[x][y] = ASCII[int(len(ASCII)/256 * brightness_matrix[x][y])]
-            print(ASCII_matrix[x][y]*3, end='')
+            print(ASCII_matrix[x][y]*2, end='')
         print()
 
     print("Successfully constructed ASCII matrix!")
--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from construct import construct_pixel_matrix
 from construct import construct_brightness_matrix
 from construct import construct_ascii_matrix
 
+# ASCII characters in order of density (thickest to thinnest)
 ASCII = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft//\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 ASCII = ASCII[::-1]
 
+# Open image using Pillow Image Processing Library and resize the image to be
+# 10 times smaller
 im = Image.open("ntu_logo.png")
-im_resized = im.resize((im.width//10, im.height//10))
+im_resized = im.resize((im.width//5, im.height//5))
+
 height = im_resized.height
 width = im_resized.width
 
@@ -15,8 +19,15 @@ print("Successfully loaded image!")
 print("Image size: " + str(width) + " x " + str(height))
 print()
 
+# Unpack all the pixel values into a single List
 flattened_data = list(im_resized.getdata()) 
+
+# Construct the 2D Pixel Matrix
 pixel_matrix = construct_pixel_matrix(height, width, flattened_data)
+
+# Construct the Brightness Matrix
 brightness_matrix = construct_brightness_matrix(height, width, pixel_matrix)
+
+# Construct and print the ASCII Matrix to console
 construct_ascii_matrix(height, width, brightness_matrix, ASCII)
 
